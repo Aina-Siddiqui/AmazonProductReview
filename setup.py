7ce770d1467c8138f72c3dd725c4e39be47081ee
Aina Siddiqui <aina_1235@hotmail.co.uk>
2025-01-15 16:12:54 +0500

    requirements and installation done

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md","r",encoding="utf-8") as f:
+    long_description = f.read()
+__version__="0.0.0"
+REPO_NAME="AmazonProductReview"
+AUTHOR_USER_NAME="Aina-Siddiqui"
+SRC_REPO="reviewClassifier"
+AUTHOR_EMAIL="ainasiddiqui1235@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A Python package for classifying Amazon product reviews",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={
+        "":"src"
+    },
+    packages=setuptools.find_packages(where="src")
+)
+    
