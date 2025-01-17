e9e01aec069e9b6fa2d6df5262c8d3b3da9b067d
Aina Siddiqui <aina_1235@hotmail.co.uk>
2025-01-17 14:47:26 +0500

    data ingestion notebook added

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+from reviewClassifier.pipeline.stage_01_data_ingestion import DataIngestionPipeline
 from reviewClassifier.logging import logger
-logger.info("welcome to our custom log")
 
+STAGE_NAME="DATA INGESTION STAGE"
+try:
+    logger.info(f">>> Starting {STAGE_NAME } >>>")
+    data_ingestion_pipeline=DataIngestionPipeline()
+    data_ingestion_pipeline.main()
+    logger.info(f"{STAGE_NAME} completed successfully")
+except Exception as e:
+    raise e
---
+++ src/reviewClassifier/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,13 @@
+from reviewClassifier.config.configuration import ConfigurationManager
+from reviewClassifier.components.data_ingestion import DataIngestion
+from reviewClassifier.logging import logger
+
+class DataIngestionPipeline:
+    def __init__(self):
+        pass
+    def main(self):
+        config=ConfigurationManager()
+        data_ingestion_config=config.get_data_ingestion_config()
+        data_ingestion=DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
